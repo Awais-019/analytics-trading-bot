5a2adba5356b708f9f677cf382db35872b58d327
Muhammad Awais <awaistopper20@gmail.com>
2023-05-31 20:06:25 +0500

    Added colors.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,32 @@ export default <Partial<Config>>{
       fontFamily: {
         sans: ["Rebond-Grotesque", ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        primary: {
+          black: "#000000",
+          white: "#ffffff",
+          tango: "f37e25",
+          "doge-blue": "#25b6f4",
+          "cinnabar-error": "#e64f22",
+          "atlantis-success": "#7edb3a",
+          buttercup: "#f3b126",
+          "visa-white-light": "#fcf9f8",
+          "nightrider-dark": "#120c08",
+          "wafer-neu": "dcd1c8",
+        },
+        neutrals: {
+          gray: "#919191",
+          dawn: "#a09e9c",
+          cloudy: "#b1aba5",
+          tide: "#c0b7af",
+          "silver-rust": "cec3ba",
+          "wafer-m": "#dcd1c8",
+          "pearl-bush": "e7ddd5",
+          "dawn-pink": "f1eae4",
+          "spring-wood": "#f9f5f1",
+          "vista-white": "fcf9f8",
+        },
+      },
     },
   },
 };
