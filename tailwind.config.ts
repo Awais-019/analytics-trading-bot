effdacb5e361aa0943b551690a72728b35910e76
Muhammad Awais <awaistopper20@gmail.com>
2023-05-31 18:42:53 +0500

    Installed and configured tailwindcss.

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+import defaulTheme from "tailwindcss/defaultTheme";
+
+export default <Partial<Config>>{
+  content: [
+    "./components/**/*.{vue,js,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./composables/**/*.{js,ts}",
+    "./plugins/**/*.{js,ts}",
+    "./App.{js,ts,vue}",
+    "./app.{js,ts,vue}",
+    "./Error.{js,ts,vue}",
+    "./error.{js,ts,vue}",
+    "./content/**/*.md",
+  ],
+  extend: {},
+};
